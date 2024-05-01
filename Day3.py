7c5ef688ae6476107c8a6fe7d35efb8da31b1c95
dauvannam321 <dauvannam321@gmail.com>
2024-05-01 20:52:37 +0700

    day 3

---
+++ Day3.py
@@ -0,0 +1,10 @@
+name = "Alice"
+
+# Câu 1
+print(f"{name} is a great teacher!")
+
+# Câu 2
+name = "ms Taylor"
+print("Title case:", name.title())
+print("Upper case:", name.upper())
+print("Lower case:", name.lower())
